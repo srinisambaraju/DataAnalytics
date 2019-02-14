17e37edee48fb6d8ae329ecb0bdaf857897f38ce
srinisambaraju <srini.sambaraju@gmail.com>
2019-02-14 16:31:26 -0700

    code modified and added comments

--- TestTensorFlow.py
+++ TestTensorFlow.py
@@ -19,12 +19,18 @@ np.savez('TF_Test', inputs=generated_inputs, targets=generated_targets)
 input_size = 2
 output_size = 1
 
+# Each object we create using Tensorflow library would do nothing unless explicitly told to. It rather describes the
+# logic of the algorithm
+
+# when you say None below it doesn't mean the data has no dimension, it means that we don't have to specify it
+
 inputs = tf.placeholder(tf.float32, [None, input_size])
 targets = tf.placeholder(tf.float32, [None, output_size])
 
+# Variables preserve their values across iterations, while placeholders dont
+# Usually we feed the data through placeholders and vary the variables
 
 weights = tf.Variable(tf.random_uniform([input_size, output_size], minval=-0.1, maxval=0.1))
-
 biases = tf.Variable(tf.random_uniform([output_size], minval=-0.1, maxval=0.1))
 
 outputs = tf.matmul(inputs, weights) + biases
@@ -35,12 +41,13 @@ optimize = tf.train.GradientDescentOptimizer(learning_rate=0.05).minimize(mean_l
 
 sess = tf.InteractiveSession()
 
+# Initializing variabile
+
 initialize = tf.global_variables_initializer()
 
 sess.run(initialize)
 
 training_data = np.load('TF_Test.npz')
-
 
 for i in range(100):
     _, curr_loss = sess.run([optimize, mean_loss], feed_dict={inputs: training_data['inputs'],
