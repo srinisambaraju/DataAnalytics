d8dd6306f0a84e319ab94a2d850e27a3404f32f8
srinisambaraju <srini.sambaraju@gmail.com>
2019-04-03 09:12:23 -0600

    added new test

--- unittests/test_youtubetest1.py
+++ unittests/test_youtubetest1.py
@@ -1,15 +1,8 @@
 import unittest
-import xmlrunner
 from youtuber import Youtuber
 
-# A nice video on unit testing https://www.youtube.com/watch?v=Knq_3gvB9lU
 
-# Another nice video on unit testing which also talks about mocking https://www.youtube.com/watch?v=6tNS--WetLI
-
-# Jenkins python integration https://www.youtube.com/watch?v=iGtM_OP01FU
-
-
-class Testyoutuber(unittest.TestCase):
+class Testyoutuber1(unittest.TestCase):
 
     def test_initialize_class_with_no_args(self):
         with self.assertRaises(TypeError):
@@ -20,6 +13,3 @@ class Testyoutuber(unittest.TestCase):
         self.assertEqual('Allthingsautomated', youtuber.get_name())
         self.assertEqual('http://www.youtube.com', youtuber.get_channel())
 
-
-if __name__ == '__main__':
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='./test_results'))
